d845e3236c621398864479b35540e11e9ea3d8c2
Hansin Susatya <hansinsusatya@gmail.com>
2018-12-27 18:01:52 +0700

    Finished gallery

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { gallery_list } from '../../data/galleries';
 
 @Component({
-  selector: 'app-gallery',
-  templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+	selector: 'app-gallery',
+	templateUrl: './gallery.component.html',
+	styleUrls: [ './gallery.component.scss' ]
 })
 export class GalleryComponent implements OnInit {
+	private galleries: any;
+	constructor() {
+		this.galleries = gallery_list;
+	}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
---
+++ src/data/galleries.ts
@@ -0,0 +1,58 @@
+export const gallery_list = [
+	{
+		name: 'Gallery Image 1',
+		image: 'assets/img/gal-1.jpeg'
+	},
+	{
+		name: 'Gallery Image 2',
+		image: 'assets/img/gal-2.jpeg'
+	},
+	{
+		name: 'Gallery Image 3',
+		image: 'assets/img/gal-3.jpeg'
+	},
+	{
+		name: 'Gallery Image 4',
+		image: 'assets/img/gal-4.jpeg'
+	},
+	{
+		name: 'Gallery Image 5',
+		image: 'assets/img/gal-5.jpeg'
+	},
+	{
+		name: 'Gallery Image 6',
+		image: 'assets/img/gal-6.jpeg'
+	},
+	{
+		name: 'Gallery Image 7',
+		image: 'assets/img/gal-7.jpeg'
+	},
+	{
+		name: 'Gallery Image 8',
+		image: 'assets/img/gal-8.jpeg'
+	},
+	{
+		name: 'Gallery Image 9',
+		image: 'assets/img/gal-9.jpeg'
+	},
+	{
+		name: 'Gallery Image 10',
+		image: 'assets/img/gal-10.jpeg'
+	},
+	{
+		name: 'Gallery Image 11',
+		image: 'assets/img/gal-11.jpeg'
+	},
+	{
+		name: 'Gallery Image 12',
+		image: 'assets/img/gal-12.jpeg'
+	},
+	{
+		name: 'Gallery Image 13',
+		image: 'assets/img/gal-13.jpeg'
+	},
+	{
+		name: 'Gallery Image 14',
+		image: 'assets/img/gal-14.jpeg'
+	}
+];
