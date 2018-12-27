6424f57fc52a55d2757fb002749ff7834074c85b
Hansin Susatya <hansinsusatya@gmail.com>
2018-12-27 18:38:22 +0700

    Finished footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+	selector: 'app-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: [ './footer.component.scss' ]
 })
 export class FooterComponent implements OnInit {
+	private menus: any;
+	constructor() {
+		this.menus = [
+			'Find your dream home',
+			'Request proposal',
+			'Download home planner',
+			'Contact us',
+			'Submit your property',
+			'Come work with us!'
+		];
+	}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
